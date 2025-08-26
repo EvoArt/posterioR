b92e04ade560ee0f52bef8a9431c4aa3208dcc44
Arthur <arn203@exeter.ac.uk>
2025-08-26 19:20:46 +0100

    hmc in julia prior to c++

---
+++ hmc.jl
@@ -0,0 +1,50 @@
+using LinearAlgebra, GLMakie, Distributions, ForwardDiff
+
+update_momentum(p,q,ϵ,∇U) = p .- (ϵ/2) * ∇U(q)
+update_position(p,q,ϵ,M⁻¹) = q .+ ϵ .* M⁻¹ * p
+function update_pq!(p,q,ϵ,∇U,M⁻¹)
+    p .= update_momentum(p,q,ϵ,∇U) 
+    q .= update_position(p,q,ϵ,M⁻¹)
+    p .= update_momentum(p,q,ϵ,∇U) 
+end
+
+U(x) = -sum(logpdf.(Normal(1,2),x))
+∇U(x) = ForwardDiff.gradient(input -> U(input),x)
+function hmc_step(q,ϵ,L,M,M⁻¹,U,∇U,H,p_dist = MvNormal(M))
+    q_proposal = copy(q)
+    p = rand(p_dist) 
+    H_curr = U(q) + 0.5 * dot(p, M⁻¹ * p)
+    for l in 1:L
+        update_pq!(p,q_proposal,ϵ,∇U,M⁻¹)
+    end
+    H_proposal =U(q_proposal) + 0.5 * dot(p, M⁻¹ * p)
+    prob = min(1, exp(H_curr - H_proposal))
+
+    accept = prob >= rand()
+
+    if accept
+        return q_proposal,H_proposal 
+    else
+        return q,H_curr 
+    end
+end
+function hmc(n_iter = 1000,n = 1,ϵ = 0.1,L = 40,M = I(n),M⁻¹ = M^-1,U=U,∇U=∇U,p_dist = MvNormal(M))
+    q = randn(n)
+    qs = []
+    q,H = hmc_step(q,ϵ,L,M,M⁻¹,U,∇U,0.0)
+    for i in 1:n_iter
+        q,H =  hmc_step(q,ϵ,L,M,M⁻¹,U,∇U,H)
+        push!(qs,q...)
+    end
+    return qs
+end
+
+samples =  hmc(10000)
+μ,σ = round.([mean(samples),std(samples)],sigdigits =3)
+fig = Figure()
+ax = Axis(fig[1,1], title = "trace")
+lines!(ax,samples)
+ax = Axis(fig[1,2], title = "density: μ = $μ, σ = $σ")
+density!(ax,samples)
+
+
